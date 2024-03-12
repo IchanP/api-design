ac31d3831a37c7d695bff7aee83788befe38bd61
Pontus Grandin <smeaurix@gmail.com>
2024-03-12 15:38:32 +0100

    tsx dependencies and tsconfig

--- src/server.ts
+++ src/server.ts
@@ -7,20 +7,22 @@ import express from "express";
 import helmet from "helmet";
 import logger from "morgan";
 import dotenv from "dotenv";
+import { connectToRedis } from "./config/redis.js";
+
 try {
-    
+
     dotenv.config()
     const app = express()
+
+    await connectToRedis(process.env.REDIS_CONNECT_STRING)
 
     // Boiler plate for security and logging
     app.use(helmet())
     app.use(express.json())
     app.use(logger('dev'))
 
-    // TODO error handling
 
-
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT || 3000, () => {
         console.log(`Server running at http://localhost:${process.env.PORT}`)
         console.log('Press Ctrl-C to terminate...')
     })
